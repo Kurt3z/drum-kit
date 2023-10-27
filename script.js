455c2dee7b218b96076925b0dc931fa0c4e013d7
Tiago Pinto <tiago.webdev10@gmail.com>
2023-10-27 22:47:17 +0100

    Add effect when key is pressed or clicked
    Created this effect using the transitionend event type.
    At the end of the CSS transition property the event is triggered, and
    the class that creates that effect is removed.

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ const drumButtons = document.querySelectorAll('.key');
 drumButtons.forEach(btn => {
     btn.addEventListener('click', function () {
         const sound = createAudio(`./assets/sounds/${btn.classList[1]}.mp3`);
+        btn.classList.add('playing');
         sound.play();
+    });
+
+    btn.addEventListener('transitionend', function (e) {
+        if (e.propertyName !== 'transform') {
+            return;
+        }
+        this.classList.remove('playing');
     });
 });
 
@@ -14,24 +22,31 @@ document.addEventListener('keydown', function (e) {
     switch (pressedKey) {
         case 'w':
             audioName = 'tom-1';
+            addClass(drumButtons[0], 'playing');
             break;
         case 'a':
             audioName = 'tom-2';
+            addClass(drumButtons[1], 'playing');
             break;
         case 's':
             audioName = 'tom-3';
+            addClass(drumButtons[2], 'playing');
             break;
         case 'd':
             audioName = 'tom-4';
+            addClass(drumButtons[3], 'playing');
             break;
         case 'j':
             audioName = 'snare';
+            addClass(drumButtons[4], 'playing');
             break;
         case 'k':
             audioName = 'kick-bass';
+            addClass(drumButtons[5], 'playing');
             break;
         case 'l':
             audioName = 'crash';
+            addClass(drumButtons[6], 'playing');
             break;
         default:
             return;
@@ -44,4 +59,8 @@ document.addEventListener('keydown', function (e) {
 function createAudio(audioName) {
     const sound = new Audio(audioName);
     return sound;
+}
+
+function addClass(element, className) {
+    element.classList.add(className);
 }
